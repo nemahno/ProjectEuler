5f6ac796bd9d6b366f568c0fe2ec6cd29e007b18
Lindelani Nemadzivhanani <lindelani.nemadzivhanani@vcontractor.co.za>
2020-01-23 19:17:47 +0200

    multiple of 5 and 3

---
+++ multiple_of_5.py
@@ -0,0 +1,18 @@
+
+"""
+Find the sum of all the multiples of 3 or 5 below limit
+"""
+
+
+def multiple_of_5_3_sum(limit):
+    _sum = 0
+
+    for i in range(limit):
+        if i % 3 == 0 or i % 5 == 0:
+            _sum += i
+    return _sum
+
+
+if __name__ == '__main__':
+    value = multiple_of_5_3_sum(1000)
+    print(value)
